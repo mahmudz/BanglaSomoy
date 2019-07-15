237fcfeb742afab343eed472d7242579483eab4d
mahmudz <mahmudbappy.pri@gmail.com>
2019-07-15 14:49:00 +0600

    Function optmization

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to `bangla-somoy` will be documented in this file
 
-## 1.0.0 - 201X-XX-XX
+## 1.0.1 - 2019-07-15
+
+- Minor changes
+- Function optimized
+
+## 1.0.0 - 2019-07-11
 
 - initial release
--- src/BanglaSomoy.php
+++ src/BanglaSomoy.php
@@ -4,148 +4,82 @@ namespace Mahmudz\BanglaSomoy;
 
 class BanglaSomoy{
 
-    /**
-     * [$bn_numbers All Bangla Numbers]
-     * @var [array]
-     */
+
     public static $bn_numbers = ["১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯", "০"];
     public static $en_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
     public static $en_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     public static $en_short_months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     public static $bn_months = ['জানুয়ারী', 'ফেব্রুয়ারী', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'অগাস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর'];
 
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
     public static $en_days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
     public static $en_short_days = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
     public static $bn_short_days = ['শনি', 'রবি', 'সোম', 'মঙ্গল', 'বুধ', 'বৃহঃ', 'শুক্র'];
     public static $bn_days = ['শনিবার', 'রবিবার', 'সোমবার', 'মঙ্গলবার', 'বুধবার', 'বৃহস্পতিবার', 'শুক্রবার'];
 
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
-    public static $en_times = array('am', 'pm');
-    public static $en_times_uppercase = array('AM', 'PM');
-    public static $bn_times = array('পূর্বাহ্ন', 'অপরাহ্ন');
 
+    public static $en_periods = array('am', 'pm');
+    public static $en_periods_uppercase = array('AM', 'PM');
+    public static $bn_periods = array('পূর্বাহ্ন', 'অপরাহ্ন');
 
 
     /**
-     * [$bn_numbers description]
-     * @var [type]
+     * [toBangla converts english date/day/numbers to bangla]
+     * @param  [string] $data [takes english inputs]
+     * @return [string]       [returns bangla string]
      */
-    public static function toBanglaNumber($number)
+    public static function toBangla($data)
     {
-        return str_replace(self::$en_numbers, self::$bn_numbers, $number);
+
+        $data = strtolower($data);
+        // Replacing numbers
+        $data = str_replace(self::$en_numbers, self::$bn_numbers, $data);
+
+        // Replacing periods
+        $data = str_replace(self::$en_periods, self::$bn_periods, $data);
+
+        // Replacing months
+        $data = str_replace(self::$en_months, self::$bn_months, $data);
+        // Replacing short months
+        $data = str_replace(self::$en_short_months, self::$bn_months, $data);
+
+        $data = title_case($data);
+        // Replacing days
+        $data = str_replace(self::$en_days, self::$bn_days, $data);
+        // Replacing short days
+        $data = str_replace(self::$en_short_days, self::$bn_short_days, $data);
+
+        return $data;
+    }
+
+    /**
+     * [toEnglishNumber Converts bangla date/day/numbers to english]
+     * @param  [string] $data [takes bangla inputs]
+     * @return [string]       [returns english string]
+     */
+
+    public static function toEnglish($data)
+    {
+        // Replacing numbers
+        $data = str_replace(self::$bn_numbers, self::$en_numbers, $data);
+
+        // Replacing periods
+        $data = str_replace(self::$bn_periods, self::$en_periods, $data);
+
+
+        // Replacing months
+        $data = str_replace(self::$bn_months, self::$en_months, $data);
+        // Replacing short months
+        $data = str_replace(self::$bn_months, self::$en_short_months, $data);
+
+        // Replacing days
+        $data = str_replace(self::$bn_days, self::$en_days, $data);
+        // Replacing short days
+        $data = str_replace(self::$bn_short_days, self::$en_short_days, $data);
+
+
+        return $data;
     }
 
 
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
-    public static function toEnglishNumber($number)
-    {
-        return str_replace(self::$bn_numbers, self::$en_numbers, $number);
-    }
-
-
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
-    public static function toBanglaDate($date)
-    {
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date = str_replace(self::$en_numbers, self::$bn_numbers, $date);
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date = str_replace(self::$en_months, self::$bn_months, $date);
-        $date = str_replace(self::$en_short_months, self::$bn_months, $date);
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date = str_replace(self::$en_days, self::$bn_days, $date);
-        $date = str_replace(self::$en_short_days, self::$bn_short_days, $date);
-        $date = str_replace(self::$en_days, self::$bn_days, $date);
-        return $date;
-    }
-
-
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
-    public static function toBanglaTime($time)
-    {
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $time = str_replace(self::$en_numbers, self::$bn_numbers, $time);
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $time = str_replace(self::$en_times, self::$bn_times, $time);
-        $time = str_replace(self::$en_times_uppercase, self::$bn_times, $time);
-        return $time;
-    }
-
-
-    /**
-     * [$bn_numbers description]
-     * @var [type]
-     */
-    public static function toBanglaDateTime($date_time)
-    {
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date_time = str_replace(self::$en_numbers, self::$bn_numbers, $date_time);
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date_time = str_replace(self::$en_months, self::$bn_months, $date_time);
-        $date_time = str_replace(self::$en_short_months, self::$bn_months, $date_time);
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date_time = str_replace(self::$en_days, self::$bn_days, $date_time);
-        $date_time = str_replace(self::$en_short_days, self::$bn_short_days, $date_time);
-        $date_time = str_replace(self::$en_days, self::$bn_days, $date_time);
-
-        /**
-         * [$bn_numbers description]
-         * @var [type]
-         */
-        $date_time = str_replace(self::$en_times, self::$bn_times, $date_time);
-        $date_time = str_replace(self::$en_times_uppercase, self::$bn_times, $date_time);
-        return $date_time;
-    }
 }
